ea08be66eb3a92c9e951a37d2cab252f4515f4af
amirovsunnat <sunnatbekamirov22@gmail.com>
2025-01-30 23:11:58 +0500

    Birinchi dars kodlari.

---
+++ lesson1.dart
@@ -0,0 +1,93 @@
+main() {
+// // String tipi
+//   String ism = "Abbos";
+
+//   // int tipi
+//   int yosh = 25;
+
+//   // double
+//   double $maosh = 500;
+
+//   bool isStudent = true;
+
+//   num son = 5;
+
+//   son = son + 15.5;
+
+//   var pc = 5;
+
+//   // const qiymatlar o'zgarmaydi
+//   const PI = 3.14;
+
+//   List<String> ismlar = ["Pam", "Chandler"];
+
+//   ismlar[0] = "Sunnat";
+
+//   print(
+//       'Sizning ismingiz: ${ismlar[0]}. Yoshingiz: $yosh.isEven. Maoshi: \$${$maosh}');
+
+//   List sonlar = [1, 2, 23.4, "sasa", false];
+
+//   dynamic $ismlar1 = [];
+
+//   Set flutterGuruhlar = {10.8, 52.5, 10.8};
+//   print(flutterGuruhlar);
+
+  // int raqam = 5;
+
+  // print(raqam.toString().runes);
+
+  // num son1 = 15;
+  // num son2 = 2.5;
+
+  // // arithmetic operations
+  // var yigindi = son1 + son2; // addition (qo'shish)
+  // var ayirma = son1 - son2; // subtraction (ayirish)
+  // var kopaytma = son1 * son2; // multiplication (ko'paytirish)
+  // var bolinma = son1 / son2; // division (bo'lish)
+  // var butunBolinma = son1 ~/ son2; // integer division (butun bo'lish)
+  // var qoldiq =
+  //     son1 % son2; // modulus -- Remainder After Integer Division (qoldiq)
+  // var unaryMinus =
+  //     -son1; // unary minus -- For reversing the sign of the expression (ishorani teskarisiga o'zgartirish)
+
+  // print("Ikki sonning yig'indisi -- $yigindi");
+  // print(
+  //     "******************************************"); // oq'ishga onson bo'lishligi uchun
+  // print("Ikki sonning ayirmasi -- $ayirma");
+  // print(
+  //     "******************************************"); // oq'ishga onson bo'lishligi uchun
+  // print("IKKI SONNING KO'PAYTMASI -- $kopaytma");
+  // print(
+  //     "******************************************"); // oq'ishga onson bo'lishligi uchun
+  // print("Ikki sonning bo'linmasi --  $bolinma");
+  // print(
+  //     "******************************************"); // oq'ishga onson bo'lishligi uchun
+  // print("Butun bo'linma --  $butunBolinma");
+  // print(
+  //     "******************************************"); // oq'ishga onson bo'lishligi uchun
+  // print("Birinchi sonni ikkinchi songa bo'lgandagi qoldiq --  $qoldiq");
+  // print(
+  //     "******************************************"); // oq'ishga onson bo'lishligi uchun
+  // print("Birinchi songa qarama-qarshi son -- $unaryMinus");
+  // print(
+  //     "******************************************"); // oq'ishga onson bo'lishligi uchun'
+
+  // String surname = "Micheal";
+
+  // print(surname * 3);
+
+  // int son = 5;
+
+  // // son++;
+  // print(son--);
+  // print(son);
+
+  // // son = son + 14;
+  // son ~/= 14;
+  // print(son);
+
+  // print(5 != 5 || "5"=="6");
+  int a = 14;
+  print(a is int);
+}
